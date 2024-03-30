21babc2064c385a605f736bbba04f6134699324e
ashutoshsharma58 <91483575+ashutoshsharma58@users.noreply.github.com>
2024-03-30 15:10:51 +0530

    Create Sentence.java

---
+++ Summarizer/Sentence.java
@@ -0,0 +1,27 @@
+package Summarizer;
+
+public class Sentence implements Comparable<Sentence>{
+    int docno;
+    int sentenceno;
+    float score;
+    int sentence_id;
+    public int compareTo(Sentence a) {
+        Float x = new Float(score);
+        Float y = new Float(a.score);
+        return -x.compareTo(y);
+    }
+    public String toString() {
+        return String.valueOf(score);
+    }
+    public Sentence(int docno, int sentenceno, int intersec) {
+        this.docno = docno;
+        this.sentenceno = sentenceno;
+        this.score = intersec;
+    }
+    public Sentence(int docno, int sentenceno, int intersec, int id) {
+        this.docno = docno;
+        this.sentenceno = sentenceno;
+        this.score = intersec;
+        this.sentence_id = id;
+    }
+}
